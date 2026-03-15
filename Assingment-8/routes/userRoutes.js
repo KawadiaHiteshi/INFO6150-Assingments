1e0f1d15692202eef678da6f79ce45e2c67a9d75
KawadiaHiteshi <kawadia.h@northeastern.edu>
2026-03-15 15:02:15 -0400

    usererouter js file chnages

---
+++ Assingment-8/routes/userRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+const upload = require("../middleware/uploadMiddleware");
+
+const {
+  createUser,
+  editUser,
+  deleteUser,
+  getAllUsers,
+  uploadImage,
+} = require("../controllers/userController");
+
+router.post("/create", createUser);
+router.put("/edit", editUser);
+router.delete("/delete", deleteUser);
+router.get("/getAll", getAllUsers);
+router.post("/uploadImage", upload.single("image"), uploadImage);
+
+module.exports = router;
