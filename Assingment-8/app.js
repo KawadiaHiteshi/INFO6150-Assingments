8d16692cf8c69e6bce1aec75c91d07ec65baf7ff
KawadiaHiteshi <kawadia.h@northeastern.edu>
2026-03-28 16:36:09 -0400

    assingment 10 chnages

--- Assingment-8/app.js
+++ Assingment-8/app.js
@@ -9,7 +9,10 @@ const swaggerDocument = YAML.load("./swagger/swagger.yaml");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || "http://localhost:3000",
+  credentials: true,
+}));
 app.use(express.json());
 app.use("/images", express.static("images"));
 
